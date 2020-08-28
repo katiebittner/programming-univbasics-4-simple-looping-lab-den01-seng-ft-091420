5883d5235922f4e33359fb1fa592b51d4d19712f
Katie B <katiebittner1@gmail.com>
2020-08-28 17:30:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ def loop_message_five_times(string)
     counter+=1
   end
 end
+
+def loop_message_n_times(string, integer)
+  counter = 0
+  while counter<=integer
+    puts string
+    counter+=1 
+  end
+end
