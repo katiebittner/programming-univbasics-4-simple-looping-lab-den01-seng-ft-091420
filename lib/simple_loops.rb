e332799046281cc792c0b18495aabd77bae42e61
Katie B <katiebittner1@gmail.com>
2020-08-28 17:35:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,7 +25,7 @@ end
 def return_string_array(array)
   counter = 0 
   new_array = []
-  while counter<=array.length 
+  while counter<array.length 
     new_array.push(array[counter].to_s)
     counter+=1
   end
